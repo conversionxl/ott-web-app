b3f6f03598580e6ecfff0f9aab1c93071ae86cd5
Christiaan Scheermeijer <christiaan@videodock.com>
2024-07-24 17:17:09 +0200

    refactor: add className prop to header components

--- packages/ui-react/src/components/Header/Header.tsx
+++ packages/ui-react/src/components/Header/Header.tsx
@@ -7,11 +7,12 @@ type TypeHeader = 'static' | 'fixed';
 
 type Props = {
   headerType?: TypeHeader;
+  className?: string;
   searchActive: boolean;
 };
 
-const Header = ({ children, headerType = 'static', searchActive }: PropsWithChildren<Props>) => {
-  const headerClassName = classNames(styles.header, styles[headerType], {
+const Header = ({ children, className, headerType = 'static', searchActive }: PropsWithChildren<Props>) => {
+  const headerClassName = classNames(styles.header, styles[headerType], className, {
     [styles.searchActive]: searchActive,
   });
 
